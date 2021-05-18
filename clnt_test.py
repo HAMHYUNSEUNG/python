5fda89fd4556d932aa8e35e70981b8f1c4048dfe
HAMHYUNSEUNG <hyun366720@naver.com>
2021-05-18 18:40:08 +0900

    Default Changelist

---
+++ clnt_test.py
@@ -0,0 +1,33 @@
+import socket
+import os
+
+
+
+HOST = '127.0.0.1'
+PORT = 9090
+
+#소켓 객체 생성
+clnt_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#지정한 HOST와 PORT를 사용하여 서버에 접속
+clnt_sock.connect((HOST,PORT))
+
+print("채팅방에 입장했습니다! 서버에게 물어보세요!(나가기 : q)")
+
+while True:
+    # 사용자에게 메시지 받기
+    clnt_msg = input("\n나 : ")
+    #메시지 전송
+    if clnt_msg =='q':
+        clnt_sock.sendall("q".encode())
+        break
+    clnt_sock.sendall(clnt_msg.encode())
+
+    #서버로부터 에코 메시지 수신
+    data = clnt_sock.recv(1024)
+    #수신 메시지 출력
+    print('서버 메시지 : ', data.decode())
+
+
+#소켓 종료
+clnt_sock.close()
--- echo_serv.py
+++ echo_serv.py
@@ -3,16 +3,16 @@ import socket
 HOST = '127.0.0.1'
 PORT = 9090
 
-#소켓 객체 생성 / IPv4, tcp 사용
+# 소켓 객체 생성 / IPv4, tcp 사용
 serv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-#bind 함수(주소할당)
+# bind 함수(주소할당)
 serv_sock.bind((HOST, PORT))
 
-#접속허용
+# 접속허용
 serv_sock.listen()
 
-#accept 대기 / 접속 시 연결소켓 리턴
+# accept 대기 / 접속 시 연결소켓 리턴
 
 '''
 *개념 참고 메모 
@@ -20,27 +20,28 @@ serv_sock.listen()
 여기서 conn는 연결에서 데이터를 보내고 받을 수 있는 새로운 소켓 객체이고, 
 address는 연결의 다른 끝에 있는 소켓에 바인드 된 주소입니다.
 '''
-clnt_sock , addr = serv_sock.accept()
+clnt_sock, addr = serv_sock.accept()
 
-#접속한 클라이언트 주소 출력 
-print('Connected by : \n', addr)
+# 접속한 클라이언트 주소 출력
+print("클라이언트 접속!\n")
+print('접속 클라이언트 주소 : ', addr)
 
-
-#클라이언트 메시지 수신대기 
-while 1:
+# 클라이언트 메시지 수신대기
+while True:
     data = clnt_sock.recv(1024)
 
-    #빈 문자면 종료
+    # 빈 문자면 종료
     if not data:
         break
-    
-    #수신 받은 문자열 출력 
-    print('client addr : ',addr, '\n client say : ', data.decode())
+    elif data.decode() == 'q':
+        break
+    # 수신 받은 문자열 출력
+    print('\n클라이언트 : ', data.decode())
 
-    #받은문자 클라이언트에게 전송(에코)
-    clnt_sock.sendall(data)
+    msg = input("나 : ")
+    # 받은문자 클라이언트에게 전송(에코)
+    clnt_sock.sendall(msg.encode())
 
-
-#소켓닫기
+# 소켓닫기
 clnt_sock.close()
 serv_sock.close()
