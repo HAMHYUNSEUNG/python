0f2b71ce66e68e96bb99e34acdee3ab34ad30138
hanel <71063663+hanel617@users.noreply.github.com>
2021-05-18 15:35:29 +0900

    Delete first_test.py

--- first_test.py
+++
@@ -1,6 +0,0 @@
-#vscode github 연동 테스트 페이지 입니다 
-print("first test python in github#2")
-print("test")
-
-#사무실 컴퓨터 push 후 노트북 push test
-#test결과 같은 파일에 대해 push 하면 에러난다. push를 자유롭게 하는건 다른 파일에 대해서 아니면 branch를 쓰던가 
